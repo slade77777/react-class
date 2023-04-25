6c990860c8f79f506e9b33fa1da5af7aa9a98c1c
cuongdang <cuongdx@bfast.dev>
2023-04-25 20:32:33 +0700

    share via context

---
+++ src/components/ListProfile.tsx
@@ -0,0 +1,66 @@
+import {useRef, useState} from 'react'
+import '../App.css'
+import Profile, {ProfileType} from "./Profile";
+const listProfile: Array<ProfileType> = [
+  {
+    id: 1,
+    fullname: 'A',
+    age: 12,
+    dob: "1994",
+    classValue: "class A",
+    slogan: "sologan 1",
+  },
+  {
+    id: 2,
+    fullname: 'b',
+    age: 14,
+    dob: "1994",
+    classValue: "class A",
+    slogan: "sologan 1",
+  },
+  {
+    id: 3,
+    fullname: 'c',
+    age: 16,
+    dob: "1994",
+    classValue: "class A",
+    slogan: "sologan 1",
+  }
+]
+function ListProfile() {
+  const [listAgeLiked, setListAgeLiked] = useState(0);
+  const [listProfileLiked, setListProfileLiked] = useState<Array<ProfileType>>([]);
+  const changeLikeNumber = (position: number, isLike: boolean, totalAge: number) => {
+    let copyListProfileLiked = [...listProfileLiked];
+    const idLiked = listProfile[position].id;
+    if (copyListProfileLiked.find(item => item.id === idLiked)) {
+      copyListProfileLiked = copyListProfileLiked.filter(item => item.id !== idLiked)
+      setListAgeLiked(listAgeLiked - totalAge)
+    } else {
+      copyListProfileLiked = [...copyListProfileLiked, listProfile[position]];
+      setListAgeLiked(listAgeLiked + totalAge);
+    }
+    setListProfileLiked(copyListProfileLiked);
+  }
+
+  const changeBonusAges = (position: number) => {
+    let copyListProfileLiked = [...listProfileLiked];
+    const idLiked = listProfile[position].id;
+    if (copyListProfileLiked.find(item => item.id === idLiked)) {
+      setListAgeLiked(listAgeLiked + 1)
+    }
+  }
+
+  return (
+    <div>
+      <p>Tổng số tuổi đã like: {listAgeLiked}</p>
+      {listProfile.map((item, index) => <Profile
+        id={item.id}
+        maxAge={listAgeLiked} position={index} setBonusAges={changeBonusAges}
+        changeLike={(val: boolean, totalAge: number) => changeLikeNumber(index, val, totalAge)} key={index} fullname={item.fullname}
+        age={item.age} dob={item.dob} classValue={item.classValue} slogan={item.slogan} /> )}
+    </div>
+  )
+}
+
+export default ListProfile
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,12 @@
-import {FC, useState} from "react";
+import {FC, useContext, useState} from "react";
+import { TeenagerAgeContext } from "../App";
 
 export type ProfileType = {
   id: number, fullname: string, age: number, dob: string, classValue: string, slogan?: string, isLiked?: boolean
 }
 
-const Profile: FC<ProfileType & {changeLike: (val: boolean) => void; setBonusAges: (id: number, age: number) => void; maxAge: number, position: number}> =
-  ({fullname, age, dob, classValue, slogan, changeLike, setBonusAges, maxAge, position}) => {
+const Profile: FC<ProfileType & {changeLike: (val: boolean) => void; setBonusAges: (id: number, age: number) => void; maxAge: number, position: number }> =
+  ({ fullname, age, dob, classValue, slogan, changeLike, setBonusAges, maxAge, position}) => {
   const [isLiked, setLike] = useState<boolean>(false);
   const [bonusAge , setBonusAge] = useState(0);
   function addOneAge() {
@@ -13,7 +14,10 @@ const Profile: FC<ProfileType & {changeLike: (val: boolean) => void; setBonusAge
     setBonusAges(position, age + bonusAge + 1)
   }
 
-  function handleLike() {
+  const ageProvider = useContext<{age: number, changeAge: (val: number) => void}>(TeenagerAgeContext);
+    console.log(ageProvider);
+
+    function handleLike() {
     setLike(isLiked === false ? true : false);
     changeLike(!isLiked, age + bonusAge);
   }
@@ -21,12 +25,13 @@ const Profile: FC<ProfileType & {changeLike: (val: boolean) => void; setBonusAge
     return <div>
       { age + bonusAge === maxAge && <p>Lớn đầu nhất</p>}
       <p>Full name: {fullname}</p>
-      <p>age: {age + bonusAge}</p>
+      <p>age: {age + bonusAge} { age + bonusAge < ageProvider.age ? '(Tuổi teen)' : ''}</p>
       <p>dob: {dob}</p>
       <p>class: {classValue}</p>
       <p>slogan: {slogan}</p>
       <button onClick={addOneAge}>cộng 1 tuổi</button>
       <button onClick={handleLike}>{isLiked ? 'Liked' : 'Like'}</button>
+      <button onClick={() => ageProvider.changeAge(ageProvider.age + 2)}>Tăng tuổi teen lên 2</button>
     </div>
 
 }
